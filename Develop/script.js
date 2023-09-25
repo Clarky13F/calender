f09197c8b4be8dabd9cb47d702bdaf99fdb16941
Brandon Clark <clarkbrandon141@gmail.com2666brandon clark$ git config --global user.email clarkbrandon141@gmail.com>
2023-09-25 18:51:23 -0500

    added javaScript and code

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,45 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+const hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+const currentHour = dayjs().hour();
+
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  const saveButtons = document.querySelectorAll(".saveBtn");
+  const timeBlocks = document.querySelectorAll(".time-block");
+
+  // Loop through each hour and set the time block properties
+  for (var i = 0; i < hours.length; ++i) {
+
+    // Set class for each time block
+    if (hours[i] < currentHour) {
+      timeBlocks[i].classList.add("past");
+    } else if (hours[i] == currentHour) {
+      timeBlocks[i].classList.add("present");
+    } else {
+      timeBlocks[i].classList.add("future");
+    }
+
+    // Get localStorage entry for each time block
+    const localStorageText = localStorage.getItem(hours[i]);
+    const timeBlockDescription = timeBlocks[i].querySelector(".description");
+    timeBlockDescription.value = localStorageText;
+
+    // Add a click event listener to each save button and save to localStorage
+    saveButtons[i].addEventListener("click", function (event) {
+      const timeBlock = event.target.closest(".time-block");
+      const hour = timeBlock.id.replace("hour-", "");
+      const timeBlockDescription = timeBlock.querySelector(".description");
+
+      localStorage.setItem(hour, timeBlockDescription.value);
+    });
+  }
+
+  updateClock();
+  setInterval(updateClock, 1000);
 });
+
+function updateClock() {
+  const clockElement = document.getElementById("clock");
+  const now = dayjs();
+  const formattedDate = now.format('dddd, MMMM D, YYYY hh:mm:ss A');
+
+  clockElement.textContent = formattedDate;
+}
